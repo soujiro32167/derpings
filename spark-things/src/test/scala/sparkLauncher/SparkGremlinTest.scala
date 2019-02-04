4f8a5840d008293c170b3afc72c0399ce8d6e1dd
Eli Kasik <soujiro32167@gmail.com>
2019-02-04 12:23:08 -0500

    Success of getting persisted RDDs!

--- spark-things/src/test/scala/sparkLauncher/SparkGremlinTest.scala
+++ spark-things/src/test/scala/sparkLauncher/SparkGremlinTest.scala
@@ -6,6 +6,8 @@ import gremlin.scala._
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.SparkSession
 import org.apache.tinkerpop.gremlin.driver.remote.DriverRemoteConnection
+import org.apache.tinkerpop.gremlin.hadoop.Constants
+import org.apache.tinkerpop.gremlin.process.computer.GraphComputer
 import org.apache.tinkerpop.gremlin.process.computer.traversal.TraversalVertexProgram
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource
 import org.apache.tinkerpop.gremlin.spark.process.computer.SparkGraphComputer
@@ -13,37 +15,56 @@ import org.apache.tinkerpop.gremlin.structure.util.GraphFactory
 import org.scalatest.{FlatSpec, Matchers}
 import org.apache.tinkerpop.gremlin.process.traversal.AnonymousTraversalSource._
 import org.apache.tinkerpop.gremlin.spark.structure.Spark
+import org.apache.tinkerpop.gremlin.spark.structure.io.PersistedInputRDD
 
 import scala.collection.JavaConverters._
 
 class SparkGremlinTest extends FlatSpec with Matchers {
 
-  "spark with gremlin" should "do its thing" in {
+  behavior of "spark with gremlin"
+
+  ignore should "do its thing" in {
     val graph: Graph = GraphFactory.open("src/test/resources/hadoop-gryo.properties")
     val g: GraphTraversalSource = graph.traversal().withComputer(classOf[SparkGraphComputer])
     //val spark = SparkSession.builder().getOrCreate()
 
-    //graph.compute().program()
-    val program = TraversalVertexProgram.build()
-      .traversal(g, "gremlin-groovy", "g.V()")
-      .create(graph)
-    val result = graph.compute().program(program).submit().get()
+    println("traversal results:")
+    println(g.V().count().next())
+    println(g.V().out().out().values("name").next())
 
-    println("memory: " + result.memory())
-    //println("" + result.graph().)
+    println("rdds:")
 
-    result.close()
-
-    //println(g.V().count().next())
-    //println(g.V().out().out().values("name").next())
-
-    //val rdds = Spark.getRDDs.asScala
-    //rdds foreach println
+    val rdds = Spark.getRDDs.asScala
+    SparkContext.getOrCreate().getPersistentRDDs
+    rdds foreach println
 
     //Spark.close()
   }
 
-  "spark with gremlin" should "use remote data" in {
+  it should "run a traversal vertex program" in {
+
+    val graph: Graph = GraphFactory.open("src/test/resources/hadoop-gryo.properties")
+    val g: GraphTraversalSource = graph.traversal().withComputer(classOf[SparkGraphComputer])
+
+    graph.compute(classOf[SparkGraphComputer])
+      .result(GraphComputer.ResultGraph.NEW)
+      .persist(GraphComputer.Persist.EDGES)
+      .program(TraversalVertexProgram.build.traversal(
+        graph.traversal.withComputer(classOf[SparkGraphComputer]),
+        "gremlin-groovy",
+        "g.V().valueMap(true)").create(graph)).submit.get
+
+    val rdds = Spark.getRDDs.asScala
+    rdds foreach { rdd =>
+      rdd foreach { item =>
+        println(item)
+      }
+    }
+
+    //Spark.close()
+  }
+
+  ignore should "use remote data" in {
     val g: GraphTraversalSource =
       traversal()
         .withRemote(DriverRemoteConnection.using("localhost", 8182, "g"))
@@ -53,7 +74,7 @@ class SparkGremlinTest extends FlatSpec with Matchers {
     println(g.V().out().out().values("name").next())
   }
 
-  "spark with gremlin" should "use remote spark" in {
+  ignore should "use remote spark" in {
     val graph: Graph = GraphFactory.open("src/test/resources/hadoop-gryo-cluster-submit.properties")
     val g: GraphTraversalSource = graph.traversal().withComputer(classOf[SparkGraphComputer])
 
